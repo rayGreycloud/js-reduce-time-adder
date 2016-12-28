566686ffd8dc855c927eb2421dd2c815da1941ba
rayGreycloud <rayGreycloud@gmail.com>
2016-12-28 12:30:28 -0800

    Compute total number of seconds and convert to hours:minutes:seconds

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ const seconds = timeNodes
     const [mins, secs] = timeCode.split(':').map(parseFloat);
     return (mins * 60) + secs;
     console.log(mins, secs);
-  });
+  })
+  // Compute total number of seconds
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+  let secondsLeft = seconds;
+  // Calculate hours left
+  const hours = Math.floor(secondsLeft / 3600);
+  // Calculate remaining seconds
+  secondsLeft = secondsLeft % 3600;
+  // Calculate minutes left
+  const minutes = Math.floor(secondsLeft / 60);
+  secondsLeft = secondsLeft % 60;
+
+  console.log(`Total Time: ${hours}:${minutes}:${secondsLeft}`);
