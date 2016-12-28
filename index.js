408e33f7377bbe26993e3e9a8abff3ed3ef951ff
rayGreycloud <rayGreycloud@gmail.com>
2016-12-28 12:39:34 -0800

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 // Using JS to add up time strings
 
-// Get time strings from li's
+// Get array of time strings from li's
 const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 
 // Convert each time string to numeric seconds
 const seconds = timeNodes
+  // Get time from data-time attribute
   .map(node => node.dataset.time)
+  // Separate minutes and seconds and convert to numbers
   .map(timeCode => {
     const [mins, secs] = timeCode.split(':').map(parseFloat);
     return (mins * 60) + secs;
-    console.log(mins, secs);
   })
   // Compute total number of seconds
   .reduce((total, vidSeconds) => total + vidSeconds);
-
+  // Get total seconds
   let secondsLeft = seconds;
   // Calculate hours left
   const hours = Math.floor(secondsLeft / 3600);
-  // Calculate remaining seconds
+  // Calculate remaining seconds to convert
   secondsLeft = secondsLeft % 3600;
   // Calculate minutes left
   const minutes = Math.floor(secondsLeft / 60);
+  // Calculate seconds left
   secondsLeft = secondsLeft % 60;
 
   console.log(`Total Time: ${hours}:${minutes}:${secondsLeft}`);
